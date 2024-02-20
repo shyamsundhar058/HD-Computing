075160de65c6de6dcebe3fbeaeec1b9c934dbc5f
= <=>
2024-02-20 13:57:11 -0800

    config file added to the repo

---
+++ Config.py
@@ -0,0 +1,15 @@
+directory = ""
+dataset = "Simulation Measurements.xlsx"
+
+D = 10000
+mu = 0
+sigma = 1
+binarize = 0
+binaryModel = 1
+sparse = 0
+s = 0.1
+#if binarize make rate 1
+rate = 0.037
+#rate = 1
+
+#rate==0
